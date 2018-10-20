85a93ad5b61ff8e4bfe0e84834c1ac1ebeb78b64
MrIrre <38958739+MrIrre@users.noreply.github.com>
2018-10-21 01:13:42 +0500

    Are changed to static.

--- src/chatbot/AnswerRepository.java
+++ src/chatbot/AnswerRepository.java
@@ -9,60 +9,53 @@ import java.util.Random;
  * Хранилище возможных ответов бота на какие-либо запросы пользователя(Будет дополняться).
  */
 public class AnswerRepository {
-    private ArrayList<String> HelloAnswers;
-    private ArrayList<String> RightAnswers;
-    private ArrayList<String> WrongAnswers;
+    private static ArrayList<String> HelloAnswers = AnswerReader.ParseAnswersFromFile("Answers/Hello.txt");
+    private static ArrayList<String> RightAnswers = AnswerReader.ParseAnswersFromFile("Answers/Right.txt");
+    private static ArrayList<String> WrongAnswers = AnswerReader.ParseAnswersFromFile("Answers/Wrong.txt");
 
     private static Random random = new Random();
 
-
-    public AnswerRepository(){
-        HelloAnswers = AnswerReader.ParseAnswersFromFile("Answers/Hello.txt");
-        RightAnswers = AnswerReader.ParseAnswersFromFile("Answers/Right.txt");
-        WrongAnswers = AnswerReader.ParseAnswersFromFile("Answers/Wrong.txt");
-    }
-
-    public String getRandomHello(){
+    public static String getRandomHello(){
         return HelloAnswers.get(random.nextInt(HelloAnswers.size()));
     }
 
-    public String getRandomRightAnswer(){
+    public static String getRandomRightAnswer(){
         return RightAnswers.get(random.nextInt(RightAnswers.size()));
     }
 
-    public String getRandomWrongAnswer(){
+    public static String getRandomWrongAnswer(){
         return WrongAnswers.get(random.nextInt(WrongAnswers.size()));
     }
 
-    public String getScoreString() {
+    public static String getScoreString() {
         return "Количество очков: ";
     }
 
-    public String getHealthString(){
+    public static String getHealthString(){
         return "Ваши HP, сэр: ";
     }
 
-    public String getWinString(){
+    public static String getWinString(){
         return "Победа! Слава и хвала победителю! :D";
     }
 
-    public String getLoseString() {
+    public static String getLoseString() {
         return "Вы проиграли... Не расстраивайтесь, в следующий раз обязательно получится:)";
     }
 
-    public String getGameOverString(){
+    public static String getGameOverString(){
         return "Игра окончена! Спасибо за игру:3 Для начала новой игры напиши \"/start\".";
     }
 
-    public String getHPEndedString(){
+    public static String getHPEndedString(){
         return "ХП нема. Это конец.";
     }
 
-    public String getAnswerEndedString() {
+    public static String getAnswerEndedString() {
         return "Вы ответили на все вопросы! Поздравляю!";
     }
 
-    public String getWrongRequestAnswerString(){
+    public static String getWrongRequestAnswerString(){
         return "Сорян, не понял. Давай ещё раз.";
     }
 }
