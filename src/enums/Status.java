80160b1e91c851b3ec2e7569b6b5d878474437f7
MrIrre <38958739+MrIrre@users.noreply.github.com>
2018-10-26 23:08:32 +0500

    Deleted StatusEnum for HelpReading

--- src/enums/Status.java
+++ src/enums/Status.java
@@ -4,9 +4,8 @@ package enums;
  * Енумы статуса пользователя для правильной обработки его запроса.
  */
 public enum Status {
-    StartGame,
-    AnswerTheQuestion,
-    GameOver,
-    HelpReading,
-    ChoosingTopic
+    Starting,
+    ChoosingGame,
+    Playing,
+    GameOver
 }
