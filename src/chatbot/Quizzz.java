5c9deab9a66bdbee80bcbb159b3bcc5bff6e81cc
MrIrre <38958739+MrIrre@users.noreply.github.com>
2018-10-26 23:04:46 +0500

    Added running from console with args

--- src/chatbot/Quizzz.java
+++ src/chatbot/Quizzz.java
@@ -1,13 +1,75 @@
 package chatbot;
 
+import interfaces.Input;
+import interfaces.Output;
+
+import org.apache.commons.cli.*;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+
 /**
- * Точка входа в приложение.
+ * Точка входа в приложение. Обработка входных аргументов для запуска определённой версии чат-бота.
  */
 public class Quizzz {
-
     public static void main(String[] args) throws Exception {
-        Console curConsole = new Console();
+        Options options = new Options();
+
+        Option vkBotArgOption = new Option("vk", "vkbot",
+                false, "run vk bot version");
+        vkBotArgOption.setRequired(false);
+        options.addOption(vkBotArgOption);
+
+        Option telegramBotArgOption = new Option("t", "telegram",
+                false, "run telegram bot version");
+        telegramBotArgOption.setRequired(false);
+        options.addOption(telegramBotArgOption);
+
+        Option consoleArgOption = new Option("c", "console",
+                false, "run console version");
+        consoleArgOption.setRequired(false);
+        options.addOption(consoleArgOption);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        Object curMode = null;
+
+        try {
+            cmd = parser.parse(options, args);
+            if (cmd.getOptions().length == 0){
+                System.out.println("See help.");
+                System.exit(1);
+            }
+
+            if (cmd.getOptions().length > 1){
+                System.out.println("Enter ONLY one argument!");
+                System.exit(1);
+            }
+
+            if (cmd.hasOption("vk")){
+                System.out.println("VkBot Version is running.");
+                curMode = new VkVersion();
+            }
+            else if (cmd.hasOption("c")){
+                System.out.println("Console Version is running.");
+                curMode = new Console();
+            }
+            else if (cmd.hasOption("t")){
+                System.out.println("TelegramBot Version is running.");
+                ApiContextInitializer.init();
+                curMode = new TelegramBot();
+                TelegramBotsApi botApi = new TelegramBotsApi();
+                botApi.registerBot((TelegramBot)curMode);
+            }
+
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("Quizzz.java", options);
+            System.exit(1);
+        }
+
         MainLoop mainLoop = new MainLoop();
-        mainLoop.runMainLoop(curConsole, curConsole);
+        mainLoop.runMainLoop((Input)curMode, (Output)curMode);
     }
 }
