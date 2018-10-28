a8368cf26f1c9133199a3e94f4c4f01b91824e7c
MrIrre <38958739+MrIrre@users.noreply.github.com>
2018-10-28 17:19:57 +0500

    Added situation strings and handling for it

--- src/chatbot/RequestHandler.java
+++ src/chatbot/RequestHandler.java
@@ -10,13 +10,15 @@ import java.util.ArrayList;
  * Логика бота. Обработка текущего запроса, в зависимости от состояния пользователя, отправившего данный запрос.
  */
 public class RequestHandler {
+    public static final String EXIT_STRING = "/exit";
+
     /**
      * Метод, обрабатывающий запрос, который возвращает ответ на данный запрос.
      * @param inputString Строка запроса.
      * @param user Пользователь, запрос которого, обрабатывается.
      * @return Строки с ответом.
      */
-    ArrayList<String> getAnswer(String inputString, User user) throws Exception {
+    public ArrayList<String> getAnswer(String inputString, User user) throws Exception {
         ArrayList<String> answerList = new ArrayList<>();
 
         /*
@@ -26,26 +28,33 @@ public class RequestHandler {
 
         if (user.state == Status.Starting) {
             if (!inputString.equals("/start")){
-                answerList.add(AnswerRepository.getWrongRequestAnswerString());
+                answerList.add(AnswerRepository.getWrongStartRequestAnswerString());
             }
             else{
                 answerList.add(AnswerRepository.getRandomAnswer(AnswerRepository.helloAnswers));
                 user.state = Status.ChoosingGame;
+                answerList.add("Чтобы выйти в любой момент из игры напиши \"" + EXIT_STRING + "\"");
                 answerList.add(AnswerRepository.getGamesString());
             }
         }
 
         else if (user.state == Status.ChoosingGame){
-            if (inputString.equals("1")){
-                user.game = new QuizLogic();
-                user.state = Status.Playing;
-            }
-            else if (inputString.equals("2")){
-                user.game = new HangmanLogic();
-                user.state = Status.Playing;
-            }
-            else {
-                answerList.add(AnswerRepository.getWrongRequestAnswerString());
+            switch (inputString) {
+                case EXIT_STRING:
+                    user.state = Status.Starting;
+                    answerList.add(AnswerRepository.getExitString());
+                    break;
+                case "1":
+                    user.game = new QuizLogic();
+                    user.state = Status.Playing;
+                    break;
+                case "2":
+                    user.game = new HangmanLogic();
+                    user.state = Status.Playing;
+                    break;
+                default:
+                    answerList.add(AnswerRepository.getWrongRequestAnswerString());
+                    break;
             }
         }
 
@@ -53,7 +62,14 @@ public class RequestHandler {
          * Если пользователь играет, то его запросы обрабатывает определённая игра.
          */
         if (user.state == Status.Playing){
-            answerList.addAll(user.game.handleGameRequest(inputString, user));
+            if (inputString.equals(EXIT_STRING)){
+                user.game = null;
+                user.state = Status.Starting;
+                answerList.add(AnswerRepository.getExitString());
+            }
+            else{
+                answerList.addAll(user.game.handleGameRequest(inputString, user));
+            }
         }
 
         return answerList;
